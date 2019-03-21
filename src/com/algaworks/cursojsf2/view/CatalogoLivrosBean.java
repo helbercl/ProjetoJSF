a014b338c857cd816db4259ac2e98316ac60fcd9
Helber Cardoso Lemos <info.helber@gmail.com>
2019-03-21 18:08:43 -0300

    refs 210320191808 estudo mensagens e tabela de dados da Fase III - principais componentes.

--- src/com/algaworks/cursojsf2/view/CatalogoLivrosBean.java
+++ src/com/algaworks/cursojsf2/view/CatalogoLivrosBean.java
@@ -8,10 +8,13 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.faces.application.FacesMessage;
+import javax.faces.application.FacesMessage.Severity;
 //import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 //import javax.faces.bean.NoneScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ValueChangeEvent;
 //import javax.faces.bean.ViewScoped;
@@ -35,31 +38,46 @@ public class CatalogoLivrosBean implements Serializable {
 	private Log log;
 	private List<Livros> listLivros;
 	private List<Log> listLogs;
-	private List<Livros> livrosPesquisados;	
+	private List<Livros> livrosPesquisados;
 
 	public CatalogoLivrosBean() {
 		this.livro = new Livros();
 		this.listLivros = new ArrayList<Livros>();
 		this.listLogs = new ArrayList<Log>();
 		log = new Log();
-		livrosPesquisados= new ArrayList<Livros>();
+		livrosPesquisados = new ArrayList<Livros>();
 	}
-	
+
 	private void validarCadastro() {
-		if ("".equals(this.livro.getTitulo())) {
-			this.livro.setTitulo("Titulo Não informado");
+		try {
+			if ("".equals(this.livro.getTitulo()) || this.livro.getTitulo() == null) {
+				// this.livro.setTitulo("Titulo Não informado");
+				this.adicionarMensagens("frm_catalogo_livros:ipt_titulo", FacesMessage.SEVERITY_INFO,
+						"Titulo Não Informado!", "Favor , informar titulo do Livro!");
+			}
+			if ("".equals(this.livro.getAutores()) || this.livro.getAutores() == null) {
+				// this.livro.setAutores("Autor Não Informado");
+				this.adicionarMensagens("frm_catalogo_livros:ipt_autor", FacesMessage.SEVERITY_INFO, "Autor Não Informado!", "Favor, informar o autor do livro!");
+			}
+			if ("".equals(this.livro.getAssuntos()) || this.livro.getAssuntos()==null) {
+				//this.livro.setAssuntos("Assunto Não Informado");
+				this.adicionarMensagens("frm_catalogo_livros:ipt_assunto", FacesMessage.SEVERITY_INFO, "Assunto Não Informado", "Favor, informar o assunto do livro");
+			}
+
+		} catch (Exception e) {
+			this.adicionarMensagens(null, FacesMessage.SEVERITY_ERROR, e.getMessage(), e.getLocalizedMessage());
 		}
-		if ("".equals(this.livro.getAutores())) {
-			this.livro.setAutores("Autor Não Informado");
-		}
-		if ("".equals(this.livro.getAssuntos())) {
-			this.livro.setAssuntos("Assunto Não Informado");
-		}
+
 	}
 
 	public void inserir() {
 		validarCadastro();
-		this.listLivros.add(this.livro);
+		FacesContext context = FacesContext.getCurrentInstance();
+		if (!context.getMessages().hasNext()) {
+			this.listLivros.add(this.livro);
+			this.adicionarMensagens(null, FacesMessage.SEVERITY_INFO,"Inserido com Sucesso!", "Cadastro Realizado com Sucesso!");
+		}
+		
 	}
 
 	public void excluir() {
@@ -69,11 +87,12 @@ public class CatalogoLivrosBean implements Serializable {
 	public void limpar() {
 		this.livro = new Livros();
 	}
-	
+
 	public void buscarLivros(ValueChangeEvent event) {
 		this.livrosPesquisados.clear();
 		for (Livros livros : listLivros) {
-			if ( livros.getTitulo()!=null && livros.getTitulo().toLowerCase().startsWith(event.getNewValue().toString().toLowerCase())) {
+			if (livros.getTitulo() != null
+					&& livros.getTitulo().toLowerCase().startsWith(event.getNewValue().toString().toLowerCase())) {
 				livrosPesquisados.add(livros);
 			}
 		}
@@ -95,6 +114,12 @@ public class CatalogoLivrosBean implements Serializable {
 			return "help/help_phone?faces-redirect=true";
 		}
 
+	}
+
+	private void adicionarMensagens(String clientId, Severity severity, String summary, String detail) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		FacesMessage message = new FacesMessage(severity, summary, detail);
+		context.addMessage(clientId, message);
 	}
 
 	@PostConstruct
--- src/com/algaworks/cursojsf2/view/FormularioMensagensBean.java
+++ src/com/algaworks/cursojsf2/view/FormularioMensagensBean.java
@@ -6,11 +6,11 @@ import java.util.Calendar;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "formularioMensagem")
-@SessionScoped
+@ViewScoped
 public class FormularioMensagensBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -18,28 +18,48 @@ public class FormularioMensagensBean implements Serializable {
 	private String nome;
 	private String mail;
 	private String senha;
+	private boolean ignoreDescanso;
 
 	public void cadastrar() {
-		if (this.getNome() == "" || this.getNome() == null || this.getNome().length() < 10) {
-			this.adicionarMensagem("frm_tabelas:opl_nome", FacesMessage.SEVERITY_ERROR, "Nome Incompleto",
-					"Favor informar seu nome completo.");
-		}
-		if (this.hojeDiaDescanso()) {
+		if (this.hojeDiaDescanso() && !this.ignoreDescanso) {
+			// clientID é null quando o controle não tem componente na tela- mensagem
+			// global.
 			this.adicionarMensagem(null, FacesMessage.SEVERITY_WARN, "Hoje é dia de descanso.",
-					"Não pode trabalahr nesshar nesse dia");
+					"Não pode trabalhar nesse dia!");
+		} else {
+			if (this.getNome() == "" || this.getNome() == null || this.getNome().length() < 10) {
+				this.adicionarMensagem("frm_mensagens:ipt_nome", FacesMessage.SEVERITY_ERROR, "Nome Incompleto!",
+						"Favor informar seu nome completo!.");
+			} else if ("".equals(this.getMail()) || this.getMail() == null) {
+				this.adicionarMensagem("frm_mensagens:ipt_mail", FacesMessage.SEVERITY_ERROR, "E-mail Não informado!",
+						"Favor informar seu e-mail!");
+			} else if ("".equals(this.getSenha()) | this.getSenha() == null || this.getSenha().length() < 6) {
+				this.adicionarMensagem("frm_mensagens:ips_senha", FacesMessage.SEVERITY_ERROR, "Senha Não Informada!",
+						"Favor informar a senha !");
+			}
+		}
+		FacesContext context = FacesContext.getCurrentInstance();
+		if (!context.getMessages().hasNext()) {
+			this.adicionarMensagem(null, FacesMessage.SEVERITY_INFO, "Inserido com Sucesso",
+					"Dados Inseridos com Sucesso!");
 		}
 	}
 
 	private boolean hojeDiaDescanso() {
-		boolean isHolliday = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) == (Calendar.SATURDAY)
+		boolean isHolliday = Calendar.getInstance().get(Calendar.DAY_OF_WEEK) == (Calendar.THURSDAY)
 				|| Calendar.getInstance().get(Calendar.DAY_OF_WEEK) == (Calendar.SUNDAY);
 		return isHolliday;
 	}
 
 	private void adicionarMensagem(String clientId, Severity severity, String summary, String detail) {
-       FacesContext context = FacesContext.getCurrentInstance();//Objeto que contem o estado de cada requisição.
-       FacesMessage message = new FacesMessage(severity,summary,detail);
-       context.addMessage(clientId, message);
+		/*
+		 * clientId - Id do componente ; severity tipos de nivel mensagem (error,warn
+		 * etc...) , summary breve descrição e detail - detalhe do erro(mais
+		 * explicativa)
+		 */
+		FacesContext context = FacesContext.getCurrentInstance(); // Objeto que contem o estado de cada requisição.
+		FacesMessage message = new FacesMessage(severity, summary, detail);
+		context.addMessage(clientId, message);
 	}
 
 	public String getNome() {
@@ -64,6 +84,14 @@ public class FormularioMensagensBean implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public boolean isIgnoreDescanso() {
+		return ignoreDescanso;
+	}
+
+	public void setIgnoreDescanso(boolean ignoreDescanso) {
+		this.ignoreDescanso = ignoreDescanso;
 	}
 
 }
