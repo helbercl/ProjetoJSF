57fa483b4e3cb48323ee8b995e4d79e33014f9b0
Helber Cardoso Lemos <info.helber@gmail.com>
2019-04-02 22:02:22 -0300

    refs 020420192202 atualizar aplicação

--- src/com/algaworks/cursojsf2/view/ConsultaClienteTDBean.java
+++ src/com/algaworks/cursojsf2/view/ConsultaClienteTDBean.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import com.algaworks.cursojsf2.model.ClienteTabelaDados;
 @ManagedBean(name="consultaclientesbean")
+@ViewScoped
 public class ConsultaClienteTDBean {
    
 	 private List<ClienteTabelaDados> listaClientes = new ArrayList<ClienteTabelaDados>();
@@ -17,6 +19,12 @@ public class ConsultaClienteTDBean {
 		 this.getListaClientes().add( new ClienteTabelaDados(3, "Newton Cardoso Lemos", "Aracaju"));
 		 this.getListaClientes().add(new ClienteTabelaDados(4, "Helton Cardoso Lemos", "Aracaju"));
 		 this.getListaClientes().add(new ClienteTabelaDados(5, "Helber Cardoso Lemos", "Aracaju"));
+	 }
+	 
+	 public void salvar(){
+		 for (ClienteTabelaDados clientes : listaClientes) {
+			System.out.println(clientes.getCodigo() +"-"+ clientes.getNome());
+		}
 	 }
 
 	public List<ClienteTabelaDados> getListaClientes() {
