78a2961dc8a07e28c586c4eaefbefaf353deab70
Helber Cardoso <helber@infox.com.br>
2019-11-04 18:27:58 -0300

    Atualizar Aplicação

--- src/com/algaworks/cursojsf2/view/EstoqueBean.java
+++ src/com/algaworks/cursojsf2/view/EstoqueBean.java
@@ -1,17 +1,14 @@
 package com.algaworks.cursojsf2.view;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
 import com.algaworks.cursojsf2.logs.Logs;
-import com.sun.org.apache.xml.internal.security.Init;
 
 @ManagedBean(name = "estoqueBean")
 @RequestScoped
@@ -33,7 +30,8 @@ public class EstoqueBean {
 				listaProdutos.add(item);
 				log.logAcao(null, FacesMessage.SEVERITY_INFO, "Inserido Com Sucesso!","Cadastro Realizado com Sucesso!");
 			} else {
-				log = validarCampos();
+				//log = validarCampos();
+				return;
 			}
 		} catch (Exception e) {
 			log.logAcao(null, FacesMessage.SEVERITY_ERROR, "Erro!", "Erro de Exceção :" + e.getMessage());
@@ -46,15 +44,14 @@ public class EstoqueBean {
 			if (item.getCodigoProduto()==null && ("".equals(item.getDescricaoProduto()) || item.getDescricaoProduto()==null) 
 					&& item.getQuantidade()==null && item.getValorUnitario()==null
 					&& item.getDataChecagem()==null ) {
-				log.logAcao(null, FacesMessage.SEVERITY_INFO, "Campos Obrigatorios!",
-						"Campos Obrigatorios não preenchidos!");
+				log.logAcao(null, FacesMessage.SEVERITY_INFO, "Campos Obrigatorios!","Campos Obrigatorios não preenchidos!");
 
-			} 
-				  else if ("".equals(item.getCodigoProduto().toString()) ||
-				  item.getCodigoProduto() == null) { log.logAcao("frm_estoque:ipt_codigo",
-				  FacesMessage.SEVERITY_INFO, summaryDefault,
-				  "Codigo do Produto deve ser informado!");				  
-				  } else if ("".equals(item.getDescricaoProduto()) || item.getDescricaoProduto()==null) {
+			} 	  else if ("".equals(item.getCodigoProduto().toString()) ||
+				  item.getCodigoProduto() == null) 
+			      { log.logAcao("frm_estoque:ipt_codigo",
+				  FacesMessage.SEVERITY_INFO, summaryDefault, "Codigo do Produto deve ser informado!");				  
+				  }
+			      else if ("".equals(item.getDescricaoProduto()) || item.getDescricaoProduto()==null) {
 				  log.logAcao("frm_estoque:ipt_descricao", FacesMessage.SEVERITY_INFO, summaryDefault, "Descrição do Produto deve ser informado!");
 				  } else if (item.getQuantidade() == null) {
 				  log.logAcao("frm_estoque:ipt_quantidade", FacesMessage.SEVERITY_INFO,
@@ -68,9 +65,7 @@ public class EstoqueBean {
 				 
 
 		} catch (Exception e) {
-
 			log.logAcao(null, FacesMessage.SEVERITY_ERROR, "Erro!", "Erro Detalhado:" + e.getMessage());
-
 		}
 		return log;
 	}
@@ -85,6 +80,14 @@ public class EstoqueBean {
 
 	public void setItem(ItemEstoque item) {
 		this.item = item;
+	}
+
+	public List<ItemEstoque> getListaProdutos() {
+		return listaProdutos;
+	}
+
+	public void setListaProdutos(List<ItemEstoque> listaProdutos) {
+		this.listaProdutos = listaProdutos;
 	}
 
 }
